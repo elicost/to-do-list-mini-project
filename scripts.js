e0a8876730fa008b5e1c62b8b1ff94f118088644
elicost <140678342+elicost@users.noreply.github.com>
2025-10-31 12:12:13 -0500

    Converted list item add ability from traditional JS to jQuery (1st attempt)

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
 
 function newItem(){
+//Adding new list item:
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
 
 //javascript
 //1. Adding a new item to the list of items: 
-   let li = document.createElement("li");
-   let inputValue = document.getElementById("input").value;
-   let text = document.createTextNode(inputValue);
-   li.appendChild(text);
+//    let li = document.createElement("li");
+//    let inputValue = document.getElementById("input").value;
+//    let text = document.createTextNode(inputValue);
+//    li.appendChild(text);
 
    if (inputValue === '') {
      alert("You must write something!");
